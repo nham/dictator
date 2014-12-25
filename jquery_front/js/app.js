37cfd11ad704ed19a2420f442df44c061b86a411
nham <nick@wabbo.org>
2014-12-25 23:52:06 +0000

    Clicking portal links now properly shows/hides the page on the right

--- jquery_front/js/app.js
+++ jquery_front/js/app.js
@@ -1,1 +1,10 @@
-var x = {};
+// each "action" has a callback that is called to implement the action
+var action_cbs = {};
+action_cbs.TOGGLE_PORTAL = function(i) {
+    $('#block'+i).toggleClass("hidden");
+};
+
+$('.portal').click(function() {
+    var i = parseInt( $(this).attr('id').replace('portal', '') );
+    action_cbs.TOGGLE_PORTAL(i);
+});
